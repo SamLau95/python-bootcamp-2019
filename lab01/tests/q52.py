2e58bae00c40108a6bfe872c971436d76e8e9bb4
Sam Lau <samlau95@gmail.com>
2019-08-18 11:44:22 -0700

    lab01

---
+++ lab01/tests/q52.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q52',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> secret_number
+          42
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
