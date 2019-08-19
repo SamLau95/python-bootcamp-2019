c91601186c40f81adfbf4ebe6eb7072aa930f060
Sam Lau <samlau95@gmail.com>
2019-08-19 16:12:37 -0700

    Fix q32 tests

--- lab02/tests/q32.py
+++ lab02/tests/q32.py
@@ -21,12 +21,12 @@ test = {
         }, {
             'code': r"""
           >>> print(imdb.sort_values('Votes').head().to_string(index=False))
-           Votes  Rating                        Title  Year  Decade
-           26012     8.0                  Kumonosu-jô  1957    1950
-           28012     8.0                  Le samouraï  1967    1960
-           31003     8.1        Le salaire de la peur  1953    1950
-           32385     8.0       La battaglia di Algeri  1966    1960
-           35983     8.1  The Best Years of Our Lives  1946    1940
+          Votes  Rating                        Title  Year  Decade
+          26012     8.0                  Kumonosu-jô  1957    1950
+          28012     8.0                  Le samouraï  1967    1960
+          31003     8.1        Le salaire de la peur  1953    1950
+          32385     8.0       La battaglia di Algeri  1966    1960
+          35983     8.1  The Best Years of Our Lives  1946    1940
           """,
             'hidden': False,
             'locked': False
